dd950f0cb41eadd5b815624ecea10ae2edde8117
lroux07 <lroux07@edenschool.fr>
2024-05-06 15:49:25 +0200

    Create router

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { log } = require('console');
 const express = require('express');
 const server = express();
+const router = express.Router();
 
-server.get('/index.html', function(req, res) {
+router.get('/', (req, res) => {
     res.sendfile('./index.html');
 });
+router.get('/home', (req, res) => {
+    res.send('<h1>Welcome to the Home Page</h1>');
+});
+router.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1><p>This is the about page</p>');
+});
+
+server.use('/', router);
+
 server.listen(8000, () => {
-    console.log('Server started at `http://localhost:8000/index.html`');
+    console.log('Server started at `http://localhost:8000`');
 });
