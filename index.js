d427a772ac56d4c4a9c9abf797fce8207f0d686c
lroux07 <lroux07@edenschool.fr>
2024-05-13 15:58:41 +0200

    Router redone

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 const express = require('express');
 const server = express();
 const router = express.Router();
+server.use(express.static(__dirname + '/assets'));
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendfile('./index.html');
+    const filePath = path.join(__dirname, 'index.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur interne du serveur');
+        } else {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(data);
+        }
+    });
 });
+
 router.get('/home', (req, res) => {
-    res.sendfile('./home.html');
+    const filePath = path.join(__dirname, 'home.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur interne du serveur');
+        } else {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(data);
+        }
+    });
 });
 
 router.get('/blog', (req, res) => {
-    res.sendfile('./blog.html');
+    const filePath = path.join(__dirname, 'blog.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur interne du serveur');
+        } else {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(data);
+        }
+    });
 });
 
 router.get('/create', (req, res) => {
-    res.sendfile('./create.html');
+    const filePath = path.join(__dirname, '/form_create/create.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erreur interne du serveur');
+        } else {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(data);
+        }
+    });
 });
 
 server.use('/', router);
